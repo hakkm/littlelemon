26ab583a8d59ccc8e56cf45f3c5c3950c219a34f
Khabir <hakim1khabir2@gmail.com>
2024-03-01 21:09:21 +0100

    deploy3

--- littlelemon/settings.py
+++ littlelemon/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -126,6 +127,8 @@ STATIC_URL = 'restaurant/static/'
 STATICFILES_DIRS = [
     "restaurant/static",
 ]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'restaurant/static')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
